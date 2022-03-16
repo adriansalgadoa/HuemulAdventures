c239d46b28814b9ee8013f59e386b6bfc4490345
Adrian Salgado <adriansalgadoa@gmail.com>
2022-03-16 01:36:08 +0100

    Improve .gitignore (add Library/) and add movement and jump-once to HuemulController

--- Assets/HuemulController.cs
+++ Assets/HuemulController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class HuemulController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D rb; 
+    public float JUMPING_FORCE = 7f;
+    public float MOVING_SPEED = 5f;
+
+    private bool jumping = false;
+    
+    void Start() 
     {
-        
+	    rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+	    float horizontal = Input.GetAxisRaw("Horizontal");
+
+	    rb.velocity = new Vector2(horizontal * MOVING_SPEED, rb.velocity.y);
+
+	    if (Input.GetKey("space") && !jumping) {
+		    rb.velocity = new Vector3(0, JUMPING_FORCE);
+		    jumping = true;
+	    }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+	    // Touched the ground
+	    jumping = false;
     }
 }
