b8f9e4295b2651361148d81fc6be10aca4c6318b
Adrian Salgado <adriansalgadoa@gmail.com>
2022-03-17 22:53:32 +0100

    Add an enemy spawner, basic "enemy", dead detection

---
+++ Assets/EnemySpawnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnScript : MonoBehaviour
+{
+	public GameObject enemy;
+	public float ENEMY_SPEED = 20f;
+
+    	void Start() {
+		StartCoroutine(EnemySpawner());
+	}
+
+	IEnumerator EnemySpawner() {
+		while(true) {
+			GameObject enemyInstance = Instantiate(enemy, transform.position, Quaternion.identity);
+			Rigidbody2D enemyRb = enemyInstance.GetComponent<Rigidbody2D>();
+			enemyRb.AddForce(Vector3.left * ENEMY_SPEED);
+			float spawnTime = Random.Range(1.2f, 3f);
+			Debug.Log(spawnTime);
+			yield return new WaitForSeconds(1.2f);
+		}
+	}
+}
--- Assets/HuemulController.cs
+++ Assets/HuemulController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HuemulController : MonoBehaviour
 {
@@ -17,18 +18,30 @@ public class HuemulController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-	    float horizontal = Input.GetAxisRaw("Horizontal");
-
-	    rb.velocity = new Vector2(horizontal * MOVING_SPEED, rb.velocity.y);
+	    //float horizontal = Input.GetAxisRaw("Horizontal");
+	    //rb.velocity = new Vector2(horizontal * MOVING_SPEED, rb.velocity.y);
 
 	    if (Input.GetKey("space") && !jumping) {
 		    rb.velocity = new Vector3(0, JUMPING_FORCE);
 		    jumping = true;
 	    }
+
+	    if (Input.GetKey("down")) {
+		    rb.velocity = new Vector3(0, -JUMPING_FORCE);
+	    }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-	    // Touched the ground
-	    jumping = false;
+	    // Touched something
+	    switch(collision.gameObject.tag) {
+		    case "Ground":
+			    jumping = false;
+			    break;
+		    case "Enemy":
+			    SceneManager.LoadScene("MainScene");
+			    break;
+		    default:
+			    break;
+	    }
     }
 }
